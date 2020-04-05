5da9d76e8f3ee97ab5788f29bec9ca4ec76f284d
FawwazFaisal <63050252+FawwazFaisal@users.noreply.github.com>
2020-04-05 05:30:11 +0500

    Add files via upload

--- FlaskAPI.py
+++ FlaskAPI.py
@@ -8,29 +8,24 @@ app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 model = pickle.load(open("PredictModel.sav", 'rb'))
-def floatHourToTime(fh):
-    h, r = divmod(fh, 1)
-    m, r = divmod(r*60, 1)
-    return (int(h),int(m),int(r*60))
-def predict(dmc_name,date):
-	dmc_name = np.float(dmc_name)
-	date = np.float(date)
-	result = model.predict(np.array([[dmc_name,date]]))
-	
 
-	result_date = datetime.fromordinal(datetime(1900, 1, 1).toordinal() + int(result) - 2)
-	input_date = datetime.fromordinal(datetime(1900, 1, 1).toordinal() + int(date) - 2)
-	hour, minute, second = floatHourToTime(result % 1)
-	hour, minute, second = floatHourToTime(date % 1)
-	result_date = result_date.replace(hour=hour, minute=minute, second=second)
-	input_date = input_date.replace(hour=hour, minute=minute, second=second)
-	final_result ="Complaint Launched on: "+ str(input_date)+" will be resolved on: "+str(result_date)
+def predict(dmc_name):
+	dmc_name = np.float(dmc_name)
+	input_date = np.float(((dt.date.today().toordinal()-datetime(1900,1,1).toordinal())))
+	output_date = model.predict(np.array([[dmc_name,input_date]]))
+
+	output_date = datetime.fromordinal(datetime(1900,1,1).toordinal()+ int(output_date[0]))
+
+	output_date =str(output_date.date()).split("-")
+	output_date.reverse()
+	final_result = "/".join(output_date)
+
 	return final_result
 
 @app.route('/prediction', methods=['GET','POST'])
 def prediction():
 
-    res = predict(flask.request.args.get("dmc_name"), flask.request.args.get("date"))
+    res = predict(flask.request.args.get("dmc_name"))
     return res
 
 if __name__ == "__main__":
