db00dbc9c7b0d4127a5f5857aa817e33a9871f9c
FawwazFaisal <63050252+FawwazFaisal@users.noreply.github.com>
2020-06-05 18:04:57 +0500

    Add files via upload

--- FlaskAPI.py
+++ FlaskAPI.py
@@ -8,13 +8,14 @@ import pickle
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-model = pickle.load(open("PredictModel.sav", 'rb'))
+model = pickle.load(open("NewPredictModel.sav", 'rb'))
 
-def predict(dmc_name):
+def predict(type, dmc_name):
 	dmc_name = np.float(dmc_name)
+	type = np.float(type)
 	input_date = np.float(((dt.date.today().toordinal()-datetime(1900,1,1).toordinal())))
-	output_date = model.predict(np.array([[dmc_name,input_date]]))
-
+	output_date = model.predict(np.array([[type,dmc_name,input_date]]))
+	
 	output_date = datetime.fromordinal(datetime(1900,1,1).toordinal()+ int(output_date[0]))
 
 	output_date =str(output_date.date()).split("-")
@@ -25,9 +26,10 @@ def predict(dmc_name):
 
 @app.route('/prediction', methods=['GET','POST'])
 def prediction():
-
-    res = predict(flask.request.args.get("dmc_name"))
-    return res
+	type = flask.request.args.get("type")
+	dmc_name = flask.request.args.get("dmc_name")
+	res = predict(type, dmc_name)
+	return res
 
 if __name__ == "__main__":
     app.run(use_reloader=False, port = 5005)
